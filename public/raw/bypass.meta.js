5feec830dccba4ef35bf9fe8023b7d9d9ca3cc51
John <faysal.heshmatullah@gmail.com>
2026-04-09 05:44:24 -0300

    Create bypass.meta.js

---
+++ public/raw/bypass.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         VortixWorld Bypass
+// @namespace    afklolbypasser
+// @version      2.9
+// @description  Bypass 💩 Fr
+// @author       afk.l0l
+// @match        *://*/*
+// @icon         https://i.ibb.co/LdshK1fR/461-F6268-08-F3-4-E8A-BC73-409218-A3-F168.jpg
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-settings.js
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-logs.js
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-toast.js
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-resource.js
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-lootlink.js
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-api.js
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-tpi.js
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-luarmor.js
+// @require      https://vortixworlduserscript.vercel.app/raw/vw-spdm.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_setClipboard
+// @grant        none
+// @license      MIT
+// @run-at       document-start
+// @updateURL    https://vortixworlduserscript.vercel.app/raw/bypass.meta.js
+// @downloadURL  https://vortixworlduserscript.vercel.app/raw/bypass.user.js
+// ==/UserScript==
